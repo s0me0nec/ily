bbed8da760c251081f3f231a107e3c9c44b94f43
s0me0nec <s2021151432@firstasia.edu.ph>
2025-12-27 01:24:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 function openGift() {
-    const giftBox = document.getElementById("giftBox");
-    const letter = document.getElementById("letter");
-    
-    // Hide the gift box
-    giftBox.style.display = 'none';
-    
-    // Show the letter with a fade-in effect
-    letter.style.display = 'block';
-    setTimeout(() => {
-        letter.style.opacity = 1;
-    }, 100);
+  document.querySelector(".gift").style.display = "none";
+  document.getElementById("letter").style.display = "block";
 }
